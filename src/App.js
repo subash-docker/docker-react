6aae1da527a00cc808a462927b40e83a15bb3b70
subash <thapa.subash345@gmail.com>
2019-10-20 12:23:23 -0700

    Changed name

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.Hi I am suji.
+          Edit <code>src/App.js</code> and save to reload.Hi I am subash.
         </p>
         <a
           className="App-link"
